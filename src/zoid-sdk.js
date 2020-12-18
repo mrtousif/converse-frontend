9475a73688f186ec0b006278138780c760779a04
Tousif <tousifislampersonal@outlook.com>
2020-12-18 13:20:38 +0530

    added grpahql package and zoid url update

--- src/zoid-sdk.js
+++ src/zoid-sdk.js
@@ -4,7 +4,7 @@ import * as zoid from "zoid/dist/zoid.frameworks";
 
 const zoidComponent = zoid.create({
     tag: "converse-presentation",
-    url: "http://localhost:1234",
+    url: "https://converse.vercel.com",
     autoResize: {
         width: false,
         height: true,
